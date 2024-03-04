30bf6ee53c4b8311eded0e4810eb01b1b9f86408
DHE <ydh.postkasten@yahoo.com>
2024-03-04 10:31:28 +0100

    dev project

---
+++ Project-Time-Stamp/Vorlagen/local-storage.js
@@ -0,0 +1,14 @@
+let data = { 
+
+}
+
+dat["monday"] = [ ]
+dat["tuesday"] = [ ]
+dat["wednesday"] = [ ]
+dat["thursday"] = [ ]
+dat["friday"] = [ ]
+
+
+
+data["monday"].append({description: "Selbststudium" , duration: 1.5})
+data["monday"].push({description: "Selbststudium" , duration: 1.5})
