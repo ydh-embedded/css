2cef17e0af448dd58c101667fc11eb06a90d46b4
DHE <ydh.postkasten@yahoo.com>
2024-03-07 09:15:47 +0100

    add project

---
+++ Heinicke - Project Multible Joice/questions.js
@@ -0,0 +1,30 @@
+const questions = [
+    {
+      question: "Wie lautet die Spezifikation für die Sprache JavaScript?",
+      answers: [
+        "TypeScript",               /* 0 */
+        "ECMAScript",               /* 1 */
+        "JScript",                  /* 2 */
+        "Java",                     /* 3 */
+      ],
+      correct: [1],
+      score: 1,
+    }   
+        ,
+    {
+      question: "Welche Aussagen treffen auf eine OOP-Klasse zu?",
+      answers: [
+        "Ist ein Bauplan für Objekte",
+        "Ist eine Lerngruppe in der Schule",
+        "Definiert die Methoden und Felder von Objekten.",
+        "Definiert die Klassifikation der Programmiersprache",
+      ],
+      correct: [0, 2], // Indizes der korrekten Antworten
+      score: 1,        // Anzahl der Punkte bei richtiger Antwort
+    }
+        ,
+    
+
+  ]
+  
+  export default questions
