b852e19ba8ff0bc8e2a448113d1f7a847c70944b
DHE <ydh.postkasten@yahoo.com>
2024-03-11 10:45:07 +0100

    dev webpack.config

--- lodash/webpack.config.js
+++ lodash/webpack.config.js
@@ -2,8 +2,34 @@ const path = require('path') ;
 
 module.exports = {
         entry:  './src/index.js'    ,
+    }
+    
+    
+    const path = require('path');
+    
+    module.exports = {
+        target: 'node',
+        mode: 'development',
+        entry: './src/index.js',
         output: {
             filename:   'awesome.js' ,
             path:       path.resolve(__dirname, 'dist') ,
-        }
-}
+        }   ,
+        externals: [
+            nodeExternals({
+      modulesDir: path.join(__dirname, '../../node_modules')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+};
